7bd8df536faf4629abd1700dc00107a45dd764dc
Mike Ayala <ayala.s.mike@gmail.com>
2021-05-10 12:30:02 -0700

    completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,42 @@
+var width = 4;
+var height = 12;
+
+var area = width * height;
+console.log('value of area: ', area);
+console.log('typeof area: ', typeof area);
+
+var bill = 17;
+var payment = 25;
+
+var change = payment - bill;
+console.log('value of change:', change);
+console.log('typeof change', typeof change);
+
+var quizzes = 90;
+var midterm = 45;
+var final = 87;
+
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Mike';
+var lastName = 'Ayala';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 8.7;
+var isAcidic = pH > 7;
+console.log('value of isAcidic', isAcidic);
+console.log('typeof isAcidic', typeof isAcidic);
+
+var headCount = 9;
+var isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta', typeof isSparta);
+
+var motto = fullName;
+motto = motto + ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
