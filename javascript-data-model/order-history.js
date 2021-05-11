bcb399b8790a9a2c596bb7fbaa4d307dcf1e18be
Mike Ayala <ayala.s.mike@gmail.com>
2021-05-11 16:44:37 -0700

    fixed javascript-data-model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,75 +1,76 @@
 var orderHistory = [
   {
-    productInfo: 'JavaScript for impatient programmers by Raushmayer, Dr. Axel',
-    retailPrice: '$31.55',
-    totalPaid: '$34.00',
     orderPlaced: 'August 4, 2020',
-    dateDelivered: 'August 8, 2020',
+    total: '$34.00',
     shippedTo: 'JS Masher',
     orderNumber: '114-3941689-8772232',
-    returnWindow: 'Sep 7, 2020',
-    buyItAgain: 'true',
-    viewYourItem: 'true',
-    accessOrderDetails: 'true',
-    accessInvoice: 'true',
-    productReview: 'true',
-    productSupport: 'false',
-    directlyHandedToResident: 'false',
-    image: 'true'
+    delivered: 'August 8, 2020',
+    item: [
+      {
+        type: 'book',
+        title: 'JavaScript for impatient programmers',
+        author: 'Raushmayer, Dr. Axel',
+        price: '$31.55',
+        returnWindow: 'September 7, 2020',
+        imgURL: 'image/Javascript.png'
+      }
+    ]
   },
   {
-    productInfo: 'The Timeless Way of Building by Alexander, Christopher',
-    retailPrice: '$41.33',
-    totalPaid: '$44.53',
     orderPlaced: 'July 19, 2020',
-    dateDelivered: 'July 20, 2020',
+    total: '$44.53',
     shippedTo: 'JS Masher',
     orderNumber: '113-9984268-1280257',
-    returnWindow: 'August 19, 2020',
-    buyItAgain: 'true',
-    viewYourItem: 'true',
-    accessOrderDetails: 'true',
-    accessInvoice: 'true',
-    productReview: 'true',
-    productSupport: 'false',
-    directlyHandedToResident: 'true',
-    image: 'true'
-
+    delivered: 'July 20, 2020',
+    item: [
+      {
+        type: 'book',
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        price: '$41.33',
+        returnWindow: 'August 19, 2020',
+        imgURL: 'image/Timeless-way.png'
+      }
+    ]
   },
   {
-    productInfo: 'Gamecube Controller Adapter. Super Smash Bros witch Gamecube Adapter for WII UIEvent, PC. SUPport Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
-    retailPrice: '$15.98',
-    totalPaid: '$17.22',
     orderPlaced: 'July 4, 2020',
-    dateDelivered: 'July 7, 2020',
+    total: '$17.22',
     shippedTo: 'JS Masher',
     orderNumber: '114-2875557-9059409',
-    returnWindow: 'August 5, 2020',
-    buyItAgain: 'true',
-    viewYourItems: 'true',
-    accessOrderDetails: 'true',
-    accessInvoice: 'true',
-    productReview: 'true',
-    productSupport: 'false',
-    directlyHandedToResident: 'true',
-    image: 'true'
+    delivered: 'July 7, 2020',
+    item: [
+      {
+        type: 'hardware',
+        Name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No driver and no Lag-Gamecube Adapter',
+        price: '$15.98',
+        returnWindow: 'August 5, 2020',
+        imgURL: 'image/controller-adapter.png'
+      }
+    ]
   },
   {
-    productInfo: ['GameCube Controller-Super Smash Bros. Edition (Nintendo Switch)', 'The Art of Sql by Faroult, Stephane'],
-    retailPrice: ['$94.95', '$33.99'],
-    totalPaid: ['$138.93'],
-    orderPlaced: ['July 3, 2020'],
-    dateDelivered: ['July 5, 2020'],
-    shippedTo: ['JS Masher'],
-    orderNumber: ['113-2883177-2648248'],
-    returnWindow: ['August 4, 2020'],
-    buyItAgain: ['true'],
-    viewYourItems: ['true'],
-    accessOrderDetails: ['true'],
-    accessInvoice: ['true'],
-    productReview: ['true'],
-    productSupport: ['true'],
-    directlyHandedToResident: ['true'],
-    image: ['true']
+    orderPlaced: 'July 3, 2020',
+    total: '138.9.3',
+    shippedTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    delivered: 'July 5, 2020',
+    item: [
+      {
+        type: 'hardware',
+        name: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        price: '$94.95',
+        returnWindow: 'August 4, 2020',
+        imgURL: 'image/controller.png'
+      },
+      {
+        type: 'book',
+        title: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        price: '$33.99',
+        returnWindow: 'August 4, 2020',
+        imgURL: 'image/Art-of-SQL.png'
+      }
+    ]
   }
 ];
