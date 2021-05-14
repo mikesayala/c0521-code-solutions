ff8f929afa850cc8c4be7d5a864d94952399727f
Mike Ayala <ayala.s.mike@gmail.com>
2021-05-14 10:33:10 -0700

    removed || false at the end of isVowel

--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -1,4 +1,4 @@
 /* exported isVowel */
 function isVowel(char) {
-  return char === 'a' || char === 'A' || char === 'e' || char === 'E' || char === 'i' || char === 'I' || char === 'o' || char === 'O' || char === 'u' || char === 'U' || false;
+  return char === 'a' || char === 'A' || char === 'e' || char === 'E' || char === 'i' || char === 'I' || char === 'o' || char === 'O' || char === 'u' || char === 'U';
 }
