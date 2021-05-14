24c8933df4c6e48c7a883f1a7b42b4ea0646f227
Mike Ayala <ayala.s.mike@gmail.com>
2021-05-14 09:56:10 -0700

    wait sorry i forgot to take out the commented code this is the true completion

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,7 +1,6 @@
 /* exported getWords */
 function getWords(string) {
   var array = [];
-  // for (var i = 0; i < string.length; i++) {
   if (string.length > 0) {
     return string.split(' ');
   } else {
--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -1,12 +1,4 @@
 /* exported isVowel */
-// function isVowel(char) {
-//   var vowels = [ ];
-//   if (char === vowels) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
 function isVowel(char) {
   return char === 'a' || char === 'A' || char === 'e' || char === 'E' || char === 'i' || char === 'I' || char === 'o' || char === 'O' || char === 'u' || char === 'U' || false;
 }
