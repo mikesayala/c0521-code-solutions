a8d5ad82a3923ffdd67ff5d79b65f1a591aa55e9
Mike Ayala <ayala.s.mike@gmail.com>
2021-05-14 09:54:17 -0700

    completed changes to string-manipulation-1

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,10 +1,10 @@
 /* exported getWords */
 function getWords(string) {
   var array = [];
-  for (var i = 0; i < string.length; i++) {
-    if (string.length > 0) {
-      return string.split(' ');
-    }
+  // for (var i = 0; i < string.length; i++) {
+  if (string.length > 0) {
+    return string.split(' ');
+  } else {
+    return array;
   }
-  return array;
 }
--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -8,6 +8,5 @@
 //   }
 // }
 function isVowel(char) {
-  var array = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'];
-  return array.includes(char);
+  return char === 'a' || char === 'A' || char === 'e' || char === 'E' || char === 'i' || char === 'I' || char === 'o' || char === 'O' || char === 'u' || char === 'U' || false;
 }
