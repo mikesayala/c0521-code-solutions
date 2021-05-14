d8b60a514920c3794d8c74bae8e0419d508a499f
Mike Ayala <ayala.s.mike@gmail.com>
2021-05-13 22:38:40 -0700

    completed string-manipulation-1

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,1 +1,10 @@
 /* exported getWords */
+function getWords(string) {
+  var array = [];
+  for (var i = 0; i < string.length; i++) {
+    if (string.length > 0) {
+      return string.split(' ');
+    }
+  }
+  return array;
+}
--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -1,1 +1,13 @@
 /* exported isVowel */
+// function isVowel(char) {
+//   var vowels = [ ];
+//   if (char === vowels) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+function isVowel(char) {
+  var array = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'];
+  return array.includes(char);
+}
