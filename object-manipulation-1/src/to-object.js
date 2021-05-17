7d9a5365bdc7ae2fec5d09fde36b685c9cda443e
Mike Ayala <ayala.s.mike@gmail.com>
2021-05-17 13:15:14 -0700

    cleaned up code on toObjects src

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -2,8 +2,6 @@
 function toObject(keyValuePair) {
   var obj = {};
   var keys = keyValuePair[0];
-  for (var i = 0; i < keyValuePair.length; i++) {
-    obj[keys] = keyValuePair[i];
-  }
+  obj[keys] = keyValuePair[1];
   return obj;
 }
