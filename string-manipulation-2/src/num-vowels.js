fc55941d027708d0655d56d79a5990db6cc520f5
Mike Ayala <ayala.s.mike@gmail.com>
2021-05-18 09:57:16 -0700

    working on string-manipulation-2

--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,11 @@
 /* exported numVowels */
+function numVowels(string) {
+  var vowels = 'aAeEiIoOuU';
+  var amountOfVowels = 0;
+  for (var i = 0; i < String.length; i++) {
+    if (vowels.indexOf(string[i]) !== -1) {
+      amountOfVowels += 1;
+    }
+  }
+  return amountOfVowels;
+}
