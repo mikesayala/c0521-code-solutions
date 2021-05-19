3b183723be2decf355839df899c991903972df5b
Mike Ayala <ayala.s.mike@gmail.com>
2021-05-18 19:09:07 -0700

    in progress

---
+++ javascript-modal/main.js
@@ -0,0 +1,11 @@
+// click open modal and enable pop up
+// click NO and close the pop up
+// need to create an if statement
+//
+
+// function SurveySurprise(event) {
+//   if
+// }
+// var $openModal = document.querySelector('.open');
+// var $popUp = document.querySelector('.pop-up');
+// $openModal.addEventListener('click', placeholder);
