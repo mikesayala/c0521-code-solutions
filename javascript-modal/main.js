43b2fcdd05b136ef4728d3b9f87b0e77cca381c3
Mike Ayala <ayala.s.mike@gmail.com>
2021-05-19 08:52:01 -0700

    working on javascript-modal

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,11 +1,43 @@
 // click open modal and enable pop up
 // click NO and close the pop up
 // need to create an if statement
-//
+// When i click the blue button it should toggle the container to go black and
+// pop up to show up
+// when i click NO it should close the pop up.
 
+// var popUp = false;
 // function SurveySurprise(event) {
-//   if
+//   var $survey;
+//   if (popUp === true) {
+//     popUp = false;
+//     $survey = 'on';
+//   } else {
+//     popUp = true;
+//     $survey = 'off';
+//   }
+//   $container.className = 'container ' + $survey;
 // }
-// var $openModal = document.querySelector('.open');
-// var $popUp = document.querySelector('.pop-up');
-// $openModal.addEventListener('click', placeholder);
+
+// $popUp.addEventListener('click', SurveySurprise);
+
+var openMo = false;
+function blueButton(event) {
+  var $blueButton;
+  if (openMo === true) {
+    openMo = false;
+    $blueButton = 'off';
+  } else {
+    openMo = true;
+    $blueButton = 'on';
+  }
+  $popUp.className = 'pop-up ' + $blueButton;
+  $container.className = 'container ' + $blueButton;
+  $openModal.className = 'open ' + $blueButton;
+}
+
+var $popUp = document.querySelector('.pop-up');
+var $container = document.querySelector('.container');
+var $openModal = document.querySelector('.open');
+$openModal.addEventListener('click', blueButton);
+
+// var $survey = document.querySelector('.survey');
