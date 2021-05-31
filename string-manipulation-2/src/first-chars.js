3fc9a62cfefd096074f6b53d4c472538415be75b
Mike Ayala <ayala.s.mike@gmail.com>
2021-05-31 15:04:13 -0700

    in progress

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,4 +1,16 @@
 /* exported firstChars */
+// Max length print out first 8 characters
+// String cat = cat OK
+// ex californiaBurrito = californ
+// ex nacho = nacho
 function firstChars(length, string) {
-  return string.substr(string[length]);
+  var myStr = '';
+  var maxLength = length;
+  if (string.length < maxLength) {
+    maxLength = string.length;
+  }
+  for (var i = 0; i < maxLength; i++) {
+    myStr += string[i];
+  }
+  return myStr;
 }
--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,4 +1,10 @@
 /* exported lastChars */
 function lastChars(length, string) {
-  return string.substr(string[length]);
+  var myStr = '';
+  for (var i = string.length; i > 0; i--) {
+    if (i > length - 1) {
+      myStr = myStr + string[i];
+    }
+  }
+  return myStr;
 }
