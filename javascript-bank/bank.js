eaa80867c9efeebd56ec4992f0b611b1bb7ac5b3
Mike Ayala <ayala.s.mike@gmail.com>
2021-06-02 17:41:32 -0700

    completed javascript-bank

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -5,23 +5,39 @@ function Bank() {
 }
 
 Bank.prototype.openAccount = function (holder, balance) {
+  if (balance <= 0 || !Number.isInteger(balance)) {
+    return null;
+  } else {
+    var accountNumber = new Account(this.nextAccountNumber, holder);
+    accountNumber.deposit(balance);
+    this.accounts.push(accountNumber);
+    this.nextAccountNumber++;
+  }
+  return accountNumber.number;
+};
+// need to find account object inside this.accounts array
+// the object account.number needs to match the value of number
+// number is the parameter
+// need to loop through this.accounts
+// if no match return null
+// if there is a match return account object.
+// account object is this.accounts[i]
+// account.number of the object is
+Bank.prototype.getAccount = function (number) {
   for (var i = 0; i < this.accounts.length; i++) {
-    if (balance < 0 || !Number.isInteger(balance)) {
-      return null;
-    } else {
-      var accountNumber = new Account(this.nextAccountNumber, holder);
-      accountNumber += Bank.accounts[i].balance;
-      this.accounts.push(accountNumber);
-      this.nextAccountNumber++;
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
     }
   }
-  return this.nextAccountNumber;
+  return null;
 };
-
-Bank.prototype.getAccount = function (number) {
-
-};
-
+// need all accounts
+// need all amounts from each account
+// add the amount of all accounts together
 Bank.prototype.getTotalAssets = function () {
-
+  var balance = 0;
+  for (var i = 0; i < this.accounts.length; i++) {
+    balance += this.accounts[i].getBalance();
+  }
+  return balance;
 };
